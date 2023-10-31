62b0749052e4088a2924338274f0f42161a22429
Gustavo <gustavo.fior@sbcash.com.br>
2023-10-31 18:36:00 -0300

    did almost the whole thing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bookmarks-chrome-extension
 Chrome extension to save bookmarks without having to go to your folder in https://bookmarks.gustavofior.com
+
+## TODO:
+- save current folders in variable to no repeat request
+- improve design
+- let user switch folders
+- pay https://chrome.google.com/webstore/devconsole/register
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentFolderid;
+
 document
   .getElementById("saveBookmarkBtn")
   .addEventListener("click", function () {
@@ -5,24 +7,76 @@ document
       { active: true, lastFocusedWindow: true },
       function (tabs) {
         let currentUrl = tabs[0].url;
-        console.log(currentUrl);
-      }
+
+        document.getElementById("saveBookmarkBtn").classList.add("disabled");
+        document.getElementById("saveBookmarkBtn").innerHTML =
+            '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>';
+
+
+        fetch(
+          "https://bookmarks.gustavofior.com/api/trpc/bookmarks.create?batch=1",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              0: {
+                json: {
+                  url: currentUrl,
+                  folderId: currentFolderid,
+                },
+              },
+            }),
+          }
+        )
+          .then((response) => response.json())
+          .then((data) => {
+              document.getElementById("saveBookmarkBtn").innerHTML = data[0].result.data.json.title.split(" ")[0] + " saved!";
+              setTimeout(() => {
+                  document.getElementById("saveBookmarkBtn").innerHTML = "Save";
+                }, 2000);
+                document.getElementById("saveBookmarkBtn").classList.remove("disabled");
+            });
+        }
     );
   });
 
-const authUrl = `https://bookmarks.gustavofior.com/`;
-
-//
-document.getElementById("loginBtn").addEventListener("click", function () {
-  console.log(chrome);
-
-  chrome.identity.launchWebAuthFlow(
-    {
-      url: authUrl,
-      interactive: true,
+chrome.cookies.getAll({}, function (cookies) {
+  fetch("https://bookmarks.gustavofior.com/api/auth/session", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-CSRFToken": cookies.filter(
+        (cookie) => cookie.name === "__Host-next-auth.csrf-token"
+      )[0].value,
     },
-    function (redirectUrl) {
-      console.log(redirectUrl);
-    }
-  );
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const userId = data.user.id;
+
+      fetch(
+        `https://bookmarks.gustavofior.com/api/trpc/users.findByUserId,folders.findByUserId?batch=1&input=%7B%220%22%3A%7B%22json%22%3A%7B%22userId%22%3A%22${userId}%22%7D%7D%2C%221%22%3A%7B%22json%22%3A%7B%22userId%22%3A%22${userId}%22%7D%7D%7D
+        `,
+        {
+          headers: {
+            "X-CSRFToken": cookies.filter(
+              (cookie) => cookie.name === "__Host-next-auth.csrf-token"
+            )[0].value,
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById("folderName").classList.remove("spinner-border");
+            document.getElementById("folderName").classList.remove("spinner-border-sm");
+            document.getElementById("saveBookmarkBtn").classList.remove("disabled");
+
+          document.getElementById("folderName").innerHTML =
+            data[1].result.data.json[0].name;
+
+          currentFolderid = data[1].result.data.json[0].id;
+        });
+    });
 });
